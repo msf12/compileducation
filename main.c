aabee88052034b7cedcc88261781f4a4c632eae1
Mitchel <fieldsmitchel@gmail.com>
2016-05-26 12:54:53 -0500

    int main(int argc, char const *argv[])

---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+ * Test if the command-line argument is a file and, if so, pass it to the first
+ * stage of the lexical analyzer for reading into buffers and tokenization
+ */
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
