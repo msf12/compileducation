3e78e975e6841b63e6f52ad356eb28efc8cd0e91
Jents217 <jeremybfields@gmail.com>
2016-05-26 15:17:14 -0400

    Added standard libraries for IO and NULL

--- flags.h
+++ flags.h
@@ -1,3 +1,4 @@
 #ifndef FLAGS_H_
 #define FLAGS_H_
+unsigned int parseFlags(int argc, char const *argv[]);
 #endif
--- main.c
+++ main.c
@@ -3,7 +3,8 @@
  * stage of the lexical analyzer for reading into buffers and tokenization
  * argv = [filename, flags, output file (optional)]
  */
-
+#include <stdlib.h>
+#include <stdio.h>
 #include "flags.h"
 
 int main(int argc, char const *argv[])
@@ -11,9 +12,10 @@ int main(int argc, char const *argv[])
 	if (argc == 0)
 		return 1; //temporary code for No Arguments Error
 	FILE *source; //source is the source code of the input file
-	source = fopen(argv[0], r);
+	source = fopen(argv[0], "r");
 	if (source == NULL)
 		return -1; //temporary code for File not Found Error.
+	unsigned int flags = parseFlags(argc,argv);
 
 	return 0;
 
