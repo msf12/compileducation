144c98e886976008d036c58dc034eeacdc6874f0
Jents217 <jeremybfields@gmail.com>
2016-05-26 14:15:10 -0400

    Added file open and FNF check code
    Updated main.c to include code to open the file specified in the command
    line args--henceforth referred to by the name "source"--and to check if
    source is NULL. If it is, returns -1, which is temporary shorthand for a
    File not Found error.

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 /*
  * Test if the command-line argument is a file and, if so, pass it to the first
  * stage of the lexical analyzer for reading into buffers and tokenization
+ * argv = [filename, flags, output file (optional)]
  */
 
 int main(int argc, char const *argv[])
 {
+	FILE *source; //source is the source code of the input file
+	source = fopen(argv[0], r);
+	if (source == NULL)
+		return -1; //temporary code for File not Found Error.
 	return 0;
 }
